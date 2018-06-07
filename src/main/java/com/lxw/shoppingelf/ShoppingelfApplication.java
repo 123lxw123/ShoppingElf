8460467c9bcfd67f0a1c04b07b325d9ea40e4d96
lxw <342466322@qq.com>
2018-06-07 23:56:20 +0800

    init project

---
+++ src/main/java/com/lxw/shoppingelf/ShoppingelfApplication.java
@@ -0,0 +1,12 @@
+package com.lxw.shoppingelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingelfApplication.class, args);
+	}
+}
