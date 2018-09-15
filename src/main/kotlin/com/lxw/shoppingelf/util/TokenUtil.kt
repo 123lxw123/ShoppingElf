2db403f2d608358e4ab7289984120f1e6e3dfc9a
lxw <342466322@qq.com>
2018-09-15 23:55:03 +0800

    js path

--- src/main/kotlin/com/lxw/shoppingelf/util/TokenUtil.kt
+++ src/main/kotlin/com/lxw/shoppingelf/util/TokenUtil.kt
@@ -1,12 +1,14 @@
 package com.lxw.shoppingelf.util
 
+import org.springframework.util.ResourceUtils
+import java.io.FileReader
 import javax.script.Invocable
 import javax.script.ScriptEngineManager
 
 object TokenUtil {
     fun getHistoryToken(url: String): String {
         val engine = ScriptEngineManager().getEngineByName("nashorn")
-        engine.eval("load('src/main/resources/TokenUtil.js')")
+        engine.eval(FileReader( ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + "TokenUtil.js")))
         val invocable = engine as Invocable
         return invocable.invokeFunction("getHistoryToken", url).toString()
     }
