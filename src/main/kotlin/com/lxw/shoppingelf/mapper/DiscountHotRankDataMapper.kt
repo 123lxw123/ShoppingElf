6be282dfeef014e8dbb85fb21f266df852e8e4ad
lxw <342466322@qq.com>
2018-11-26 23:52:05 +0800

    fix sql

--- src/main/kotlin/com/lxw/shoppingelf/mapper/DiscountHotRankDataMapper.kt
+++ src/main/kotlin/com/lxw/shoppingelf/mapper/DiscountHotRankDataMapper.kt
@@ -11,7 +11,7 @@ interface DiscountHotRankDataMapper {
     @Select("SELECT * FROM discount_hot_rank_data WHERE discount_hot_rank_date=#{discountHotRankDate}")
     fun selectByDiscountHotRankDate(discountHotRankDate : String): List<DiscountHotRankDataEntity>?
 
-    @Select("SELECT * FROM discount_hot_rank_data WHERE id IN (id<#{id} AND ID IN (SELECT MAX(id) FROM discount_hot_rank_data WHERE id < #{id} GROUP BY url ORDER BY id DESC) LIMIT #{limit}")
+    @Select("SELECT * FROM discount_hot_rank_data WHERE id IN (SELECT MAX(id) FROM discount_hot_rank_data GROUP BY url) AND id < #{id} AND rank = '' ORDER BY id DESC LIMIT #{limit}")
     fun selectByIdAndLimit(id : Long, limit: Int): List<DiscountHotRankDataEntity>?
 
     @Options(useGeneratedKeys=true, keyProperty="id")
